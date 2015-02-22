7b23579e15eeaaeaa57bc2521808c745c25f65e9
tobiaskraft <kraft.tobias@gmail.com>
2015-02-22 15:36:09 +0100

    full example

--- src/main/java/com/sebuilder/interpreter/webdriverfactory/Chrome.java
+++ src/main/java/com/sebuilder/interpreter/webdriverfactory/Chrome.java
@@ -1,6 +1,5 @@
 package com.sebuilder.interpreter.webdriverfactory;
 
-import java.io.File;
 import java.util.HashMap;
 
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,24 +8,11 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Chrome implements WebDriverFactory {
     /**
-     * @param config Key/value pairs treated as required capabilities, with the exception of:
-     *               <ul>
-     *               <li>binary: path to Firefox binary to use</li>
-     *               <li>profile: path to Firefox profile to use</li>
-     *               </ul>
-     * @return A FirefoxDriver.
+     * @param config Key/value pairs treated as required capabilities.
+     * @return A ChromeDriver.
      */
     @Override
     public RemoteWebDriver make(HashMap<String, String> config) {
-//        FirefoxBinary fb = config.containsKey("binary")
-//                ? new FirefoxBinary(new File(config.get("binary")))
-//                : new FirefoxBinary();
-//
-//        HashMap<String, String> caps = new HashMap<String, String>(config);
-//        caps.remove("binary");
-//        caps.remove("profile");
-
         return new ChromeDriver(DesiredCapabilities.chrome());
-//        return new ChromeDriver(new DesiredCapabilities(caps));
     }
 }
